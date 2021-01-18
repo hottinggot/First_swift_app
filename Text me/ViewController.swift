64ba5f34f059a367f948184f6b8bea592c02ad2d
hottinggot <hottinggot@naver.com>
2021-01-18 10:25:01 +0900

    Initial Commit

---
+++ Text me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text me
+//
+//  Created by 서정 on 2021/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
